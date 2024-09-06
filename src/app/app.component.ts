24f5035099eb0a295487f215cecc78a64fa8ab97
dungtm1609 <tmd16092000@gmail.com>
2024-09-06 23:26:01 +0700

    exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {UserModel} from "./models/user.model";
+import {Country} from "./models/country.model";
 import * as jsonCountries from '../assets/countries.json';
 import * as uuid from 'uuid';
 
@@ -8,5 +9,76 @@ import * as uuid from 'uuid';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
+  title() {
+
+  }
+
+
+  userLists: UserModel[] = [];
+  listSkills: string[] = ['C#', 'PHP', 'Angular'];
+  listGenders: string[] = ['Male', 'Female'];
+
+  countries: Country[] = (jsonCountries as any).default as Country[];
+
+  selectedSkills: boolean[] = [];
+  user: UserModel = {
+    name: '',
+    email: '',
+    gender: '',
+    phone: '',
+    country: '',
+    address: '',
+    skill: [],
+    description: ''
+  };
+  editingIndex: number | null = null;
+
+  onSubmit() {
+    console.log('User data before submit:', this.user);
+
+    if (this.editingIndex !== null) {
+      this.userLists[this.editingIndex] = {...this.user};
+      this.editingIndex = null; // Clear editing index
+    } else {
+      this.userLists.push({...this.user});
+    }
+    this.resetForm();
+  }
+
+
+
+  deleteUser(index: number) {
+    this.userLists.splice(index, 1);
+  }
+
+  editUser(index: number) {
+    this.user = {...this.userLists[index]};
+    this.editingIndex = index;
+
+    this.selectedSkills = this.listSkills.map(skill => this.user.skill.includes(skill));
+  }
+
+  resetForm() {
+    this.user = {
+      name: '',
+      email: '',
+      gender: '',
+      phone: '',
+      country: '',
+      address: '',
+      skill: [],
+      description: ''
+    };
+    this.selectedSkills = [];
+  }
+
+
+  onSkillChange(index: number) {
+    this.selectedSkills[index] = !this.selectedSkills[index];
+    this.user.skill = this.listSkills
+      .filter((_, i) => this.selectedSkills[i]);
+  }
+
+
 }
