aa0737732f95d47bd646d0eb988c7d5886870838
nguyennt <nguyennt@omt.com.vn>
2024-09-07 15:42:35 +0700

    nguyennt

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,2 +1,10 @@
 export interface UserModel{
+    name: string;
+    gender: string;
+    email: string;
+    phone: string;
+    country: string;
+    address: string;
+    skills: string[];
+    description: string;
 }
