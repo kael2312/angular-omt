c7da6809245fb231b182d5d552eab940affba99c
Chung NB <nguyenbaochung9396@gmail.com>
2024-09-21 13:46:13 +0700

    feat/chungnb: Validate form với remplate-driven form

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -3,90 +3,133 @@ import {UserModel} from "../../models/user.model";
 import {Country} from "../../models/country.model";
 import * as jsonCountries from "../../../assets/countries.json";
 import * as uuid from 'uuid';
+import {FormsModule, NgForm} from "@angular/forms";
+import {UserListComponent} from "../user-list/user-list.component";
+import {NgIf, NgFor} from "@angular/common";
 
 @Component({
-    selector: 'app-user-form',
-    templateUrl: './user-form.component.html',
-    styleUrls: ['./user-form.component.css']
+  standalone: true,
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  imports: [
+    FormsModule,
+    UserListComponent,
+    NgIf,
+    NgFor,
+  ],
+  styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  ngOnInit(): void {}
 
-    constructor() {
+  // PROPERTIES
+  userLists : UserModel[] = [];
+  listSkills: string[] = ['C#', 'PHP', 'Angular']
+  countries : Country[] = (jsonCountries as any).default as Country[];
+
+  isSubmitting: boolean = false;
+
+  selectedSkills: boolean[] = [];
+  user: UserModel = {
+    id: '',
+    name: '',
+    email: '',
+    gender: '',
+    phone: '',
+    country: '',
+    address: '',
+    skill: '',
+    description: ''
+  };
+  editingUUID: string | null = null;
+
+  minLength    : number = 3;
+  maxLength    : number = 18;
+  maxLengthText: number = 255;
+  phonePattern : string = '^0\\d{9}$';
+
+  msgRequired     : string = 'Trường này là bắt buộc';
+  msgMinLength    : string = 'Độ dài tối thiểu là 3 ký tự';
+  msgMaxLength    : string = 'Độ dài tối đa là 18 ký tự';
+  msgMaxLengthText: string = 'Độ dài tối đa là 255 ký tự';
+  msgEmail        : string = 'Email không đúng định dạng';
+  msgPhonePattern : string = 'Sđt phải có 10 ký tự, và bắt đầu bằng số 0';
+
+  // METHODS
+  onSubmit(form: NgForm): void {
+    this.isSubmitting = true;
+
+    if (form.invalid) {
+      Object.keys(form.controls).forEach(field => {
+        const control = form.controls[field];
+        control.markAsTouched({ onlySelf: true });
+      })
+      return;
     }
 
-    ngOnInit(): void {
+    if (!this.isAtLeastOneSkillSelected()) {
+      return;
     }
 
-    userLists: UserModel[] = [];
-    listSkills: string[] = ['C#', 'PHP', 'Angular']
+    this.isSubmitting = false;
+    this.handleSubmit();
+  }
 
-    countries: Country[] = (jsonCountries as any).default as Country[];
+  handleSubmit() {
+    if (this.editingUUID !== null) {
+      const index = this.userLists.findIndex(user => user.id === this.editingUUID);
+      if (index !== -1) {
+        this.userLists[index] = {...this.user, id: this.editingUUID}
+      }
+      this.editingUUID = null;
+    } else {
+      const newUser = {
+        ...this.user,
+        id: uuid.v4()
+      };
+      this.userLists.push(newUser);
+    }
+    this.resetForm();
+  }
 
-    selectedSkills: boolean[] = [];
-    user: UserModel = {
-        id: '',
-        name: '',
-        email: '',
-        gender: '',
-        phone: '',
-        country: '',
-        address: '',
-        skill: '',
-        description: ''
+  deleteUser(uuid: string): void {
+    this.userLists = this.userLists.filter(user => user.id !== uuid);
+    this.resetForm();
+  }
+
+  editUser(uuid: string): void {
+    const user = this.userLists.find(user => user.id === uuid);
+    if (user) {
+      this.user = {...user};
+      this.editingUUID = uuid;
+      this.selectedSkills = this.listSkills.map(skill => this.user.skill.includes(skill));
+    }
+  }
+
+  resetForm(): void {
+    this.user = {
+      id: '',
+      name: '',
+      email: '',
+      gender: '',
+      phone: '',
+      country: '',
+      address: '',
+      skill: '',
+      description: ''
     };
-    editingUUID: string | null = null;
+    this.selectedSkills = [];
+  }
 
-    onSubmit(): void {
-        if (this.editingUUID !== null) {
-            const index = this.userLists.findIndex(user => user.id === this.editingUUID);
-            if (index !== -1) {
-                this.userLists[index] = {...this.user, id: this.editingUUID}
-            }
-            this.editingUUID = null;
-        } else {
-            const newUser = {
-                ...this.user,
-                id: uuid.v4()
-            };
-            this.userLists.push(newUser);
-        }
-        this.resetForm();
-    }
+  onSkillChange(index: number): void {
+    this.user.skill = this.listSkills
+      .filter((_, i) => this.selectedSkills[i])
+      .join(', ');
+  }
 
-    deleteUser(uuid: string): void {
-        this.userLists = this.userLists.filter(user => user.id !== uuid);
-        this.resetForm();
-    }
+  isAtLeastOneSkillSelected(): boolean {
+    return this.selectedSkills.some(skill => skill);
+  }
 
-    editUser(uuid: string): void {
-        const user = this.userLists.find(user => user.id === uuid);
-        if (user) {
-            this.user = {...user};
-            this.editingUUID = uuid;
-            this.selectedSkills = this.listSkills.map(skill => this.user.skill.includes(skill));
-        }
-    }
-
-    resetForm(): void {
-        this.user = {
-            id: '',
-            name: '',
-            email: '',
-            gender: '',
-            phone: '',
-            country: '',
-            address: '',
-            skill: '',
-            description: ''
-        };
-        this.selectedSkills = [];
-    }
-
-    onSkillChange(index: number): void {
-        this.user.skill = this.listSkills
-            .filter((_, i) => this.selectedSkills[i])
-            .join(', ');
-    }
-
-    protected readonly events = module
+  protected readonly events = module;
 }
