deb30adb6900afc0d1fa882e0ee695bbfe215047
vusociu <vu13102003@gmail.com>
2024-10-04 00:49:37 +0700

    commit code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
-
+import { Country } from './models/country.model';
+import * as jsonCountries from "../assets/countries.json";
+import { UserModel } from './models/user.model';
 
 @Component({
     selector: 'app-root',
@@ -7,6 +9,91 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    student = {
+        id: '',
+        name: '',
+        email: '',
+        gender: '',
+        phone: '',
+        country: '',
+        address: '',
+        skill: '',
+        description: '',
+        otherSkill:'',
+        love: '',
+    };
+    teacher = {
+        id: '',
+        name: '',
+        email: '',
+        gender: '',
+        phone: '',
+        country: '',
+        address: '',
+        skill: '',
+        description: '',
+        otherSkill:'',
+        love: '',
+    };
+    studentSkills = ['Nhảy', 'Viết chữ đẹp', 'Hát'];
+    teacherSkills = ['Nghiệp vụ toán', 'Nghiệp vụ tiếng việt', 'Nghiệp vụ tiếng anh'];
+    otherTeacherSkills=['Cao đẳng', 'Đại học', 'Thạc sĩ'];
+    otherStudentSkills=['English', 'Japan', 'Korean'];
+    countries: Country[] = (jsonCountries as any).default as Country[];
+    loves = ['Độc thân', 'Kết hôn', 'Đã ly hôn']
 
+    studentLists:UserModel[] = [];
+    teacherLists:UserModel[] = [];
 
+    emptyUser: UserModel={
+        id: '',
+        name: '',
+        email: '',
+        gender: '',
+        phone: '',
+        country: '',
+        address: '',
+        skill: '',
+        description: '',
+        otherSkill: '',
+        love: '',
+    };
+    editingStudent:string = '';
+    editingTeacher:string='';
+    
+    onStudentUserListsChange(updatedLists: UserModel[]) {
+        this.studentLists = updatedLists;
+        this.editingStudent='';
+    }
+
+    onTeacherUserListsChange(updatedLists: UserModel[]) {
+        this.teacherLists = updatedLists;
+        this.editingTeacher='';
+    }
+
+    editTeacher(uuid: string): void {
+        this.editingTeacher=uuid;
+        const teacherToEdit = this.teacherLists.find(teacher => teacher.id === uuid);
+        if (teacherToEdit) {
+            this.teacher = { ...teacherToEdit };
+        }
+    }
+
+    deleteTeacher(uuid: string): void {
+        this.teacherLists = this.teacherLists.filter(teacher => teacher.id !== uuid);
+        this.teacher = this.emptyUser;
+    }
+
+    editStudent(uuid: string): void {
+        this.editingStudent=uuid;
+        const studentToEdit = this.studentLists.find(teacher => teacher.id === uuid);
+        if (studentToEdit) {
+            this.student = { ...studentToEdit };
+        }
+    }
+
+    deleteStudent(uuid: string): void {
+        this.studentLists = this.studentLists.filter(teacher => teacher.id !== uuid);
+        this.student = this.emptyUser;
+    }
 }
--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {UserModel} from "../../models/user.model";
-import {Country} from "../../models/country.model";
-import * as jsonCountries from "../../../assets/countries.json";
+
 import * as uuid from 'uuid';
 import { NgForm } from '@angular/forms';
+import { Country } from 'src/app/models/country.model';
 
 @Component({
     selector: 'app-user-form',
@@ -11,20 +11,9 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
-
-    constructor() {
-    }
-
-    ngOnInit(): void {
-    }
-
-    userLists: UserModel[] = [];
-    listSkills: string[] = ['C#', 'PHP', 'Angular']
-
-    countries: Country[] = (jsonCountries as any).default as Country[];
-
-    selectedSkills: boolean[] = [];
-    user: UserModel = {
+    @Input() userType?: string;
+    
+    @Input() user: UserModel = {
         id: '',
         name: '',
         email: '',
@@ -33,66 +22,76 @@ export class UserFormComponent implements OnInit {
         country: '',
         address: '',
         skill: '',
-        description: ''
+        description: '',
+        otherSkill: '',
+        love: '',
     };
-    editingUUID: string | null = null;
+    @Input() listSkills: string[] = [];
+    @Input() countries: Country[] = [];
+    @Input() otherSkills: string[] = [];
+    @Input() loves: string[] = [];
+    @Input() editingUUID: string='';
+    
+    @Output() formSubmit: EventEmitter<any> = new EventEmitter();
+    @Output() userListsChange = new EventEmitter<any[]>();
 
+    userLists: UserModel[]=[];
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    selectedSkills: boolean[] = [];
+    skills:string='';
+    emptyUser = {
+        id: '',
+        name: '',
+        email: '',
+        gender: '',
+        phone: '',
+        country: '',
+        address: '',
+        skill: '',
+        description: '',
+        otherSkill: '',
+        love: '',
+    };
     onSubmit(form: NgForm): void {
         if(form.valid) {
-            if (this.editingUUID !== null) {
+            this.user.skill=this.skills;
+            if (this.editingUUID !== '') {
                 const index = this.userLists.findIndex(user => user.id === this.editingUUID);
                 if (index !== -1) {
                     this.userLists[index] = {...this.user, id: this.editingUUID}
                 }
-                this.editingUUID = null;
+                this.editingUUID = '';
             } else {
-                const newUser = {
+                var newUser = {
                     ...this.user,
                     id: uuid.v4()
                 };
                 this.userLists.push(newUser);
+                newUser=this.emptyUser;
             }
+            this.userListsChange.emit(this.userLists);
             this.resetForm();
         }
     }
 
-    deleteUser(uuid: string): void {
-        this.userLists = this.userLists.filter(user => user.id !== uuid);
-        this.resetForm();
-    }
-
-    editUser(uuid: string): void {
-        const user = this.userLists.find(user => user.id === uuid);
-        if (user) {
-            this.user = {...user};
-            this.editingUUID = uuid;
-            this.selectedSkills = this.listSkills.map(skill => this.user.skill.includes(skill));
-        }
-    }
-
     resetForm(): void {
-        this.user = {
-            id: '',
-            name: '',
-            email: '',
-            gender: '',
-            phone: '',
-            country: '',
-            address: '',
-            skill: '',
-            description: ''
-        };
+        this.user = this.emptyUser;
         this.selectedSkills = [];
     }
 
     onSkillChange(index: number): void {
-        this.user.skill = this.listSkills
+        this.skills = this.listSkills
             .filter((_, i) => this.selectedSkills[i])
-            .join(', ');
+            .join(',');
     }
 
     isAtLeastOneSkillSelected(): boolean {
-        return this.user.skill.trim() != ''
+        return this.skills.trim() != ''
     }
 
     protected readonly events = module
